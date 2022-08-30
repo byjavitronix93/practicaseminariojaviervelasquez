841227ba7a12cd597c9e9200639044153f60d7a9
byjavitronix93 <112446430+byjavitronix93@users.noreply.github.com>
2022-08-29 18:35:09 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal(".showcase");
+ScrollReveal().reveal(".news-cards", { delay: 500}); 
+ScrollReveal().reveal(".cards-banner-one", { delay: 500});
+ScrollReveal().reveal(".social", { delay: 500});
+ScrollReveal().reveal(".footer-links", { delay: 500});
